3bb036396d81c19025a7f68f59c2c410963b0c82
PauloMoreira26506 <pv26506@alunos.estgv.ipv.pt>
2024-06-27 17:48:38 +0100

    Update database.js

--- src/model/database.js
+++ src/model/database.js
@@ -1,10 +1,6 @@
 var Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('codestore_syvt', 'codestore_syvt_user', 'dres3ZMxuGlsa5BpsVt9l2KjQZZ7GaNZ', {
-  host: 'localhost',
-  port: '5432',
-  dialect: 'postgres'
-});
+const sequelize = new Sequelize(postgresql://codestore_syvt_user:dres3ZMxuGlsa5BpsVt9l2KjQZZ7GaNZ@dpg-cpti539u0jms73e9iae0-a.frankfurt-postgres.render.com/codestore_syvt, { protocol: 'postgres', dialect: 'postgres', dialectOptions: {ssl: {require: true, rejectUnauthorized: false}}});
 
 
 module.exports = sequelize;
