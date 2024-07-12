5ee5b414a81996354778458e2cb7aacc07c10939
PauloMoreira26506 <pv26506@alunos.estgv.ipv.pt>
2024-07-12 18:08:41 +0100

    Add files via upload

--- src/model/database.js
+++ src/model/database.js
@@ -1,7 +1,7 @@
 var Sequelize = require("sequelize");
 
 
-const sequelize = new Sequelize('postgresql://codestore_syvt_user:dres3ZMxuGlsa5BpsVt9l2KjQZZ7GaNZ@dpg-cpti539u0jms73e9iae0-a.frankfurt-postgres.render.com/codestore_syvt', { protocol: 'postgres', dialect: 'postgres', dialectOptions: {ssl: {require: true, rejectUnauthorized: false}}});
+const sequelize = new Sequelize('postgresql://codestore_l659_user:W7wDB9DcLqsQQ7CH3dnf4BW6OH2161nE@dpg-cq8m4mtds78s73dm1vcg-a.frankfurt-postgres.render.com/codestore_l659', { protocol: 'postgres', dialect: 'postgres', dialectOptions: {ssl: {require: true, rejectUnauthorized: false}}});
 
 // Método para síncronizar os model. 
 // Se não existir as tabelas na base de dados, são criadas automaticamente.
